32964677d3771d6bf58bdf025d1a7f9280b753d3
fobosdem <fobosdem@gmail.com>
2019-12-16 18:02:26 +0200

    adding some js and FrontPage

---
+++ JS HomeWork/01.js
@@ -0,0 +1,4 @@
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+for(var item of numbers){
+if(item > 50) console.log(item)
+}
---
+++ JS HomeWork/02.js
@@ -0,0 +1,18 @@
+var num1 = 3
+var num2 = 1
+var num3 = 2
+
+if(num1 < num2){
+    if(num1 < num3){
+         alert("num1 "+ num1)
+    }
+
+}
+else if(num2<num1){
+    if(num2<num3){
+         alert("num2 " + num2)
+    }
+}
+    else{
+        alert("num3 " + num3)
+    }
